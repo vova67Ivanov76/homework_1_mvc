a4a096f3cef2402ed7f2ab367d07098eee28055b
vova67Ivanov76 <115020239+vova67Ivanov76@users.noreply.github.com>
2022-10-04 19:57:47 +0500

    Add files via upload

---
+++ src/main/java/ru/springMVC/project/app/services/ProjectRepository.java
@@ -0,0 +1,13 @@
+package ru.springMVC.project.app.services;
+
+import java.util.List;
+
+public interface ProjectRepository<T> {
+    List<T> retreiveAll();
+
+    void store(T book);
+
+    boolean removeItemById(Integer bookIdToRemove);
+
+    boolean removeItem(String textToRemove);
+}
---
+++ src/main/java/ru/springMVC/project/web/dto/Book.java
@@ -0,0 +1,58 @@
+package ru.springMVC.project.web.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Book {
+    private Integer id;
+    @NotEmpty(message = "Author may not be empty")
+    private String author;
+    @NotEmpty(message = "Title may not be empty")
+    private String title;
+    @NotNull(message= "Size may not be empty")
+    @Min(value=1, message = "Size should be greater than 0")
+    private Integer size;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
