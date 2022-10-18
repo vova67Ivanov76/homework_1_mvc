737279a446b2340ffdf505ad01222d2777b1c302
vova67Ivanov76 <115020239+vova67Ivanov76@users.noreply.github.com>
2022-10-18 22:23:20 +0500

    Add files via upload

---
+++ homework1/src/main/java/ru/springMVC/project/web/controllers/BookShelfController.java
@@ -0,0 +1,53 @@
+package ru.springMVC.project.web.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
+import org.springframework.validation.BindingResult;
+import ru.springMVC.project.app.services.BookService;
+import ru.springMVC.project.web.dto.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "/books")
+@Scope("session")
+public class BookShelfController {
+
+    private Logger logger = Logger.getLogger(BookShelfController.class);
+    private BookService bookService;
+
+    @Autowired
+    public BookShelfController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/shelf")
+    public String books(Model model) {
+        logger.info(this.toString());
+        model.addAttribute("book", new Book());
+        model.addAttribute("bookList", bookService.getAllBooks());
+        return "book_shelf";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(@Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "book_shelf";
+        bookService.saveBook(book);
+        logger.info("current repository size: " + bookService.getAllBooks().size());
+        return "redirect:/books/shelf";
+    }
+
+    @PostMapping("/remove")
+    public String removeBook(@RequestParam(value = "bookIdToRemove") Integer bookIdToRemove) {
+        bookService.removeBookById(bookIdToRemove);
+        return "redirect:/books/shelf";
+    }
+}
