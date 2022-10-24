3885c3c5060fd13e531aa41658a371c1ec4a745b
vova67Ivanov76 <115020239+vova67Ivanov76@users.noreply.github.com>
2022-10-25 02:41:40 +0500

    Delete homework2/src/main directory

--- homework2/src/main/java/org/example/app/config/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package org.example.app.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
--- homework2/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO books(author, title, size) VALUES ('James Joyce', 'Ulysses', 732);
